37b78071e9267991decd491e47e0f0ef99056918
Mark Swaanenburg <85726405+mswaanen@users.noreply.github.com>
2024-03-31 15:36:05 +0200

    Optimize glprocs loading

--- r24demo/glext-stubs.cpp
+++ r24demo/glext-stubs.cpp
@@ -1,77 +1,48 @@
 #include "glext-stubs.h"
 
-PFNGLATTACHSHADERPROC glAttachShader;
-PFNGLBINDFRAMEBUFFEREXTPROC glBindFramebufferEXT;
-PFNGLCOMPILESHADERPROC glCompileShader;
-PFNGLCREATEPROGRAMPROC glCreateProgram;
-PFNGLCREATESHADERPROC glCreateShader;
-PFNGLDETACHSHADERPROC glDetachShader;
-PFNGLFRAMEBUFFERTEXTURE2DEXTPROC glFramebufferTexture2DEXT;
-PFNGLGENERATEMIPMAPPROC glGenerateMipmap;
-PFNGLGENFRAMEBUFFERSEXTPROC glGenFramebuffers;
-PFNGLLINKPROGRAMPROC glLinkProgram;
-PFNGLSHADERSOURCEPROC glShaderSource;
-PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
-PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv;   
-PFNGLDRAWARRAYSINSTANCEDEXTPROC glDrawArraysInstanced;   
-PFNGLUSEPROGRAMPROC glUseProgram;
-PFNGLGENBUFFERSPROC glGenBuffers;
-PFNGLBUFFERDATAPROC glBufferData;
-PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation;
-PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
-PFNGLBINDBUFFERPROC glBindBuffer;
-PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer;
-PFNGLVERTEXATTRIBDIVISORARBPROC glVertexAttribDivisor;
-PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
-PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
-PFNGLGENVERTEXARRAYSPROC glGenVertexArrays;
-PFNGLBINDVERTEXARRAYPROC glBindVertexArray;
-PFNGLUNIFORM3FVPROC glUniform3fv;
-PFNGLUNIFORM4FVPROC glUniform4fv;
-PFNGLUNIFORM1FVPROC glUniform1fv;
-PFNGLUNIFORM1IPROC glUniform1i;
-PFNGLACTIVETEXTUREPROC glActiveTexture;
-PFNGLBINDSAMPLERPROC glBindSampler;
-PFNGLWINDOWPOS2IPROC glWindowPos2i;
+PROC _gl_procs[33];
 
+const char *procnames =
+    "glActiveTexture\0"
+    "glAttachShader\0"
+    "glBindBuffer\0"
+    "glBindFramebufferEXT\0"
+    "glBindSampler\0"
+    "glBindVertexArray\0"
+    "glBufferData\0"
+    "glCompileShader\0"
+    "glCreateProgram\0"
+    "glCreateShader\0"
+    "glDetachShader\0"
+    "glDrawArraysInstanced\0"
+    "glEnableVertexAttribArray\0"
+    "glFramebufferTexture2DEXT\0"
+    "glGenBuffers\0"
+    "glGenerateMipmap\0"
+    "glGenFramebuffers\0"
+    "glGenVertexArrays\0"
+    "glGetAttribLocation\0"
+    "glGetProgramInfoLog\0"
+    "glGetShaderInfoLog\0"
+    "glGetUniformLocation\0"
+    "glLinkProgram\0"
+    "glShaderSource\0"
+    "glUniform1fv\0"
+    "glUniform1i\0"
+    "glUniform3fv\0"
+    "glUniform4fv\0"
+    "glUniformMatrix4fv\0"
+    "glUseProgram\0"
+    "glVertexAttribDivisor\0"
+    "glVertexAttribPointer\0"
+    "glWindowPos2i\0"
+    ;
 
 void init_glext_stubs() {
-    glAttachShader = (PFNGLATTACHSHADERPROC)wglGetProcAddress("glAttachShader");
-    glBindFramebufferEXT = (PFNGLBINDFRAMEBUFFEREXTPROC)wglGetProcAddress("glBindFramebufferEXT");
-    glCompileShader = (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
-    glCreateProgram = (PFNGLCREATEPROGRAMPROC)wglGetProcAddress("glCreateProgram");
-    glCreateShader = (PFNGLCREATESHADERPROC)wglGetProcAddress("glCreateShader");
-    glDetachShader = (PFNGLDETACHSHADERPROC)wglGetProcAddress("glDetachShader");
-    glFramebufferTexture2DEXT = (PFNGLFRAMEBUFFERTEXTURE2DEXTPROC)wglGetProcAddress("glFramebufferTexture2DEXT");
-    glGenerateMipmap = (PFNGLGENERATEMIPMAPPROC)wglGetProcAddress("glGenerateMipmap");
-    glGenFramebuffers = (PFNGLGENFRAMEBUFFERSEXTPROC)wglGetProcAddress("glGenFramebuffers");
-    glLinkProgram = (PFNGLLINKPROGRAMPROC)wglGetProcAddress("glLinkProgram");
-    glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
-    glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
-    glGenBuffers = (PFNGLGENBUFFERSPROC)wglGetProcAddress("glGenBuffers");
-    glBufferData = (PFNGLBUFFERDATAPROC)wglGetProcAddress("glBufferData");
-    glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation");
-    glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC)wglGetProcAddress("glUniformMatrix4fv");
-    glDrawArraysInstanced = (PFNGLDRAWARRAYSINSTANCEDEXTPROC)wglGetProcAddress("glDrawArraysInstanced");
-    glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
-    glGenBuffers = (PFNGLGENBUFFERSPROC)wglGetProcAddress("glGenBuffers");
-    glBufferData = (PFNGLBUFFERDATAPROC)wglGetProcAddress("glBufferData");
-    glGetAttribLocation = (PFNGLGETATTRIBLOCATIONPROC)wglGetProcAddress("glGetAttribLocation");
-    glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glEnableVertexAttribArray");
-    glBindBuffer = (PFNGLBINDBUFFERPROC)wglGetProcAddress("glBindBuffer");
-    glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)wglGetProcAddress("glVertexAttribPointer");
-    glVertexAttribDivisor = (PFNGLVERTEXATTRIBDIVISORARBPROC)wglGetProcAddress("glVertexAttribDivisor");
-    glGenVertexArrays = (PFNGLGENVERTEXARRAYSPROC)wglGetProcAddress("glGenVertexArrays");
-    glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)wglGetProcAddress("glBindVertexArray");
-    glUniform3fv = (PFNGLUNIFORM3FVPROC)wglGetProcAddress("glUniform3fv");
-    glUniform4fv = (PFNGLUNIFORM3FVPROC)wglGetProcAddress("glUniform4fv");
-    glUniform1fv = (PFNGLUNIFORM1FVPROC)wglGetProcAddress("glUniform1fv");
-    glUniform1i = (PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i");
-    glActiveTexture = (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
-    glBindSampler = (PFNGLBINDSAMPLERPROC)wglGetProcAddress("glBindSampler");
-    glWindowPos2i = (PFNGLWINDOWPOS2IPROC)wglGetProcAddress("glWindowPos2i");
-
-    // DEBUG ONLY
-    glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog");
-    glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)wglGetProcAddress("glGetProgramInfoLog");
+    int n = 0;
+    const char *p = procnames;
+    while (*p) {
+        _gl_procs[n++] = wglGetProcAddress(p);
+        p = strchr(p, '\0') + 1;
+    }
 }
--- r24demo/glext-stubs.h
+++ r24demo/glext-stubs.h
@@ -4,40 +4,78 @@
 #include <gl/gl.h>
 #include "glext.h"
 
-extern PFNGLATTACHSHADERPROC glAttachShader;
-extern PFNGLBINDFRAMEBUFFEREXTPROC glBindFramebufferEXT;
-extern PFNGLCOMPILESHADERPROC glCompileShader;
-extern PFNGLCREATEPROGRAMPROC glCreateProgram;
-extern PFNGLCREATESHADERPROC glCreateShader;
-extern PFNGLDETACHSHADERPROC glDetachShader;
-extern PFNGLFRAMEBUFFERTEXTURE2DEXTPROC glFramebufferTexture2DEXT;
-extern PFNGLGENERATEMIPMAPPROC glGenerateMipmap;
-extern PFNGLGENFRAMEBUFFERSEXTPROC glGenFramebuffers;
-extern PFNGLLINKPROGRAMPROC glLinkProgram;
-extern PFNGLUSEPROGRAMPROC glUseProgram;
-extern PFNGLSHADERSOURCEPROC glShaderSource;
-extern PFNGLGENBUFFERSPROC glGenBuffers;
-extern PFNGLBUFFERDATAPROC glBufferData;
-extern PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
-extern PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv;
-extern PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation;
-extern PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
-extern PFNGLBINDBUFFERPROC glBindBuffer;
-extern PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer;
-extern PFNGLVERTEXATTRIBDIVISORARBPROC glVertexAttribDivisor;
-extern PFNGLDRAWARRAYSINSTANCEDEXTPROC glDrawArraysInstanced;
-extern PFNGLGENVERTEXARRAYSPROC glGenVertexArrays;
-extern PFNGLBINDVERTEXARRAYPROC glBindVertexArray;
-extern PFNGLUNIFORM3FVPROC glUniform3fv;
-extern PFNGLUNIFORM4FVPROC glUniform4fv;
-extern PFNGLUNIFORM1FVPROC glUniform1fv;
-extern PFNGLUNIFORM1IPROC glUniform1i;
-extern PFNGLACTIVETEXTUREPROC glActiveTexture;
 typedef void (APIENTRYP PFNGLBINDSAMPLERPROC) (GLuint unit, GLuint sampler);
-extern PFNGLBINDSAMPLERPROC glBindSampler;
-extern PFNGLWINDOWPOS2IPROC glWindowPos2i;
 
-extern PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
-extern PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+extern PROC _gl_procs[33];
 
 void init_glext_stubs();
+
+
+#define _glprocoffs_glActiveTexture  0
+#define _glprocoffs_glAttachShader  1
+#define _glprocoffs_glBindBuffer  2
+#define _glprocoffs_glBindFramebufferEXT  3
+#define _glprocoffs_glBindSampler  4
+#define _glprocoffs_glBindVertexArray  5
+#define _glprocoffs_glBufferData  6
+#define _glprocoffs_glCompileShader  7
+#define _glprocoffs_glCreateProgram  8
+#define _glprocoffs_glCreateShader  9
+#define _glprocoffs_glDetachShader  10
+#define _glprocoffs_glDrawArraysInstanced    11
+#define _glprocoffs_glEnableVertexAttribArray  12
+#define _glprocoffs_glFramebufferTexture2DEXT  13
+#define _glprocoffs_glGenBuffers  14
+#define _glprocoffs_glGenerateMipmap  15
+#define _glprocoffs_glGenFramebuffers  16
+#define _glprocoffs_glGenVertexArrays  17
+#define _glprocoffs_glGetAttribLocation  18
+#define _glprocoffs_glGetProgramInfoLog  19
+#define _glprocoffs_glGetShaderInfoLog  20
+#define _glprocoffs_glGetUniformLocation  21
+#define _glprocoffs_glLinkProgram  22
+#define _glprocoffs_glShaderSource  23
+#define _glprocoffs_glUniform1fv  24
+#define _glprocoffs_glUniform1i  25
+#define _glprocoffs_glUniform3fv  26
+#define _glprocoffs_glUniform4fv  27
+#define _glprocoffs_glUniformMatrix4fv    28
+#define _glprocoffs_glUseProgram  29
+#define _glprocoffs_glVertexAttribDivisor  30
+#define _glprocoffs_glVertexAttribPointer  31
+#define _glprocoffs_glWindowPos2i  32
+
+
+#define glActiveTexture ((PFNGLACTIVETEXTUREPROC)_gl_procs[_glprocoffs_glActiveTexture])
+#define glAttachShader ((PFNGLATTACHSHADERPROC)_gl_procs[_glprocoffs_glAttachShader])
+#define glBindBuffer ((PFNGLBINDBUFFERPROC)_gl_procs[_glprocoffs_glBindBuffer])
+#define glBindFramebufferEXT ((PFNGLBINDFRAMEBUFFEREXTPROC)_gl_procs[_glprocoffs_glBindFramebufferEXT])
+#define glBindSampler ((PFNGLBINDSAMPLERPROC)_gl_procs[_glprocoffs_glBindSampler])
+#define glBindVertexArray ((PFNGLBINDVERTEXARRAYPROC)_gl_procs[_glprocoffs_glBindVertexArray])
+#define glBufferData ((PFNGLBUFFERDATAPROC)_gl_procs[_glprocoffs_glBufferData])
+#define glCompileShader ((PFNGLCOMPILESHADERPROC)_gl_procs[_glprocoffs_glCompileShader])
+#define glCreateProgram ((PFNGLCREATEPROGRAMPROC)_gl_procs[_glprocoffs_glCreateProgram])
+#define glCreateShader ((PFNGLCREATESHADERPROC)_gl_procs[_glprocoffs_glCreateShader])
+#define glDetachShader ((PFNGLDETACHSHADERPROC)_gl_procs[_glprocoffs_glDetachShader])
+#define glDrawArraysInstanced ((PFNGLDRAWARRAYSINSTANCEDEXTPROC)_gl_procs[_glprocoffs_glDrawArraysInstanced])
+#define glEnableVertexAttribArray ((PFNGLENABLEVERTEXATTRIBARRAYPROC)_gl_procs[_glprocoffs_glEnableVertexAttribArray])
+#define glFramebufferTexture2DEXT ((PFNGLFRAMEBUFFERTEXTURE2DEXTPROC)_gl_procs[_glprocoffs_glFramebufferTexture2DEXT])
+#define glGenBuffers ((PFNGLGENBUFFERSPROC)_gl_procs[_glprocoffs_glGenBuffers])
+#define glGenerateMipmap ((PFNGLGENERATEMIPMAPPROC)_gl_procs[_glprocoffs_glGenerateMipmap])
+#define glGenFramebuffers ((PFNGLGENFRAMEBUFFERSEXTPROC)_gl_procs[_glprocoffs_glGenFramebuffers])
+#define glGenVertexArrays ((PFNGLGENVERTEXARRAYSPROC)_gl_procs[_glprocoffs_glGenVertexArrays])
+#define glGetAttribLocation ((PFNGLGETATTRIBLOCATIONPROC)_gl_procs[_glprocoffs_glGetAttribLocation])
+#define glGetProgramInfoLog ((PFNGLGETPROGRAMINFOLOGPROC)_gl_procs[_glprocoffs_glGetProgramInfoLog])
+#define glGetShaderInfoLog ((PFNGLGETSHADERINFOLOGPROC)_gl_procs[_glprocoffs_glGetShaderInfoLog])
+#define glGetUniformLocation ((PFNGLGETUNIFORMLOCATIONPROC)_gl_procs[_glprocoffs_glGetUniformLocation])
+#define glLinkProgram ((PFNGLLINKPROGRAMPROC)_gl_procs[_glprocoffs_glLinkProgram])
+#define glShaderSource ((PFNGLSHADERSOURCEPROC)_gl_procs[_glprocoffs_glShaderSource])
+#define glUniform1fv ((PFNGLUNIFORM1FVPROC)_gl_procs[_glprocoffs_glUniform1fv])
+#define glUniform1i ((PFNGLUNIFORM1IPROC)_gl_procs[_glprocoffs_glUniform1i])
+#define glUniform3fv ((PFNGLUNIFORM3FVPROC)_gl_procs[_glprocoffs_glUniform3fv])
+#define glUniform4fv ((PFNGLUNIFORM4FVPROC)_gl_procs[_glprocoffs_glUniform4fv])
+#define glUniformMatrix4fv ((PFNGLUNIFORMMATRIX4FVPROC)_gl_procs[_glprocoffs_glUniformMatrix4fv])
+#define glUseProgram ((PFNGLUSEPROGRAMPROC)_gl_procs[_glprocoffs_glUseProgram])
+#define glVertexAttribDivisor ((PFNGLVERTEXATTRIBDIVISORARBPROC)_gl_procs[_glprocoffs_glVertexAttribDivisor])
+#define glVertexAttribPointer ((PFNGLVERTEXATTRIBPOINTERPROC)_gl_procs[_glprocoffs_glVertexAttribPointer])
+#define glWindowPos2i ((PFNGLWINDOWPOS2IPROC)_gl_procs[_glprocoffs_glWindowPos2i])
